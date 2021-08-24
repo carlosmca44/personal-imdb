8e1a470938c653f218c119b387d2c0d2e7b80f53
Carlos <57507795+carlosmca44@users.noreply.github.com>
2021-08-24 17:57:28 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+//`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${param}&r=json`
+//const axiosConfig = { method: "get", timeout: 2500 };
+
+/*{films && <div>a{films.year}</div>}
+        {loading && <p style={{ color: "blue", fontSize: "20px" }}>LOADING</p>}
+        {error && <p style={{ color: "red", fontSize: "20px" }}>ERROR</p>}
+        {console.log(response?.status)}*/
+
+const App = () => {
+  const [param, setState] = useState("");
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios({
+      url: `http://www.omdbapi.com/?apikey=cda82712&s=alita`
+    })
+      .then((response) => setList(response.data))
+      .catch((error) => console.log(error));
+  });
+
+  const handleChange = (event) => {
+    setState(event.target.value);
+  };
+
   return (
     <div className="App">
-      
+      <input type="text" value={param} onChange={handleChange} />
+      <div>
+        {list.map((item) => (
+          <div key={item.id}>{item.title}</div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
