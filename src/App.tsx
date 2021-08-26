1639b1e9b52a669b375c95f3793c5200e8c75f37
Carlos <57507795+carlosmca44@users.noreply.github.com>
2021-08-26 02:17:40 -0400

    axios ok, list.map is not a function

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import axios from "axios";
 
-//`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${param}&r=json`
-//const axiosConfig = { method: "get", timeout: 2500 };
-
-/*{films && <div>a{films.year}</div>}
-        {loading && <p style={{ color: "blue", fontSize: "20px" }}>LOADING</p>}
-        {error && <p style={{ color: "red", fontSize: "20px" }}>ERROR</p>}
-        {console.log(response?.status)}*/
-
 const App = () => {
-  const [param, setState] = useState("");
+  const [query, setQyery] = useState("");
   const [list, setList] = useState([]);
 
-  useEffect(() => {
-    axios({
-      url: `http://www.omdbapi.com/?apikey=cda82712&s=alita`
+  const fetchData = async () => {
+    await axios({
+      url: `${process.env.REACT_APP_API_URL}&s=${query}`
     })
       .then((response) => setList(response.data))
       .catch((error) => console.log(error));
-  });
+  }
 
-  const handleChange = (event) => {
-    setState(event.target.value);
+  const handleChange = (event: any) => {
+    setQyery(event.target.value);
   };
+
+  const handleButtonClick = () => {
+    fetchData()
+  }
 
   return (
     <div className="App">
-      <input type="text" value={param} onChange={handleChange} />
-      <div>
-        {list.map((item) => (
-          <div key={item.id}>{item.title}</div>
+      <input type="text" value={query} onChange={handleChange} />
+      <button onClick={handleButtonClick}>buscar</button>
+      <ul>
+        {list && list.map((item: any) => (
+          <li>{item.id}</li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
